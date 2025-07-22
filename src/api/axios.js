153717f817cb2b182b65ff3fbe6bc197e15a9030
Thai Huy <raziel.doan@gmail.com>
2025-07-22 17:48:25 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://ky-luat-le-lai-backend-production.up.railway.app/api',
+  baseURL: 'https://ky-luat-le-lai-backend-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
