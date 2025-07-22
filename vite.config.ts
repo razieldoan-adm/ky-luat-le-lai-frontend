07c70beb6152f0669f8eb39840d23ab222b42d2f
Thai Huy <raziel.doan@gmail.com>
2025-07-22 16:14:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-  proxy: {
-    '/api': {
-      target: 'https://ky-luat-le-lai-backend-production.up.railway.app', // cổng backend của bạn
-      changeOrigin: true,
-      secure: false,
-    },
-  },
-},
 })
