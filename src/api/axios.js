fb6e9cdd7b6d3576b03d6fe679e2364ce7f0bf2b
Thai Huy <raziel.doan@gmail.com>
2025-07-22 16:09:18 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
+console.log('VITE_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
+
 const instance = axios.create({
-  console.log('VITE_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
   baseURL: import.meta.env.VITE_BACKEND_URL, // Sử dụng biến môi trường backend
-  // withCredentials: true, // nếu backend yêu cầu cookie hoặc token
-  
+  // withCredentials: true,
 });
 
 export default instance;
