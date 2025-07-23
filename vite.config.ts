543ad425d4a5c5986a878ee5978cc54faa0fbc6d
Thai Huy <raziel.doan@gmail.com>
2025-07-23 14:48:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
   proxy: {
     '/api': {
-      target: 'http://ky-luat-le-lai-backend-production.up.railway.app/api', // cổng backend của bạn
+      target: 'https://ky-luat-le-lai-backend-production.up.railway.app', // cổng backend của bạn
       changeOrigin: true,
       secure: false,
     },
