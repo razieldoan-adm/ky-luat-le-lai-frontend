3410d0963b1f7e8a59e2b148d4ccfa0c3e858c15
Thai Huy <raziel.doan@gmail.com>
2025-09-14 23:31:39 +0700

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,47 +1,19 @@
-import { Outlet, Link } from "react-router-dom";
-import {
-  Drawer,
-  List,
-  ListItemButton,
-  ListItemText,
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-} from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import MainHeader from "../components/MainHeader";
+import Sidebar from "../components/Sidebar";
 
-export default function Layout() {
+export default function MainLayout() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6">Quản lý học sinh</Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Drawer variant="permanent" sx={{ width: 240 }}>
-        <List>
-          <ListItemButton component={Link} to="/">
-            <ListItemText primary="Dashboard" />
-          </ListItemButton>
-          <ListItemButton component={Link} to="/rules">
-            <ListItemText primary="Nội quy" />
-          </ListItemButton>
-          <ListItemButton component={Link} to="/view-all-violations">
-            <ListItemText primary="Xem vi phạm" />
-          </ListItemButton>
-          <ListItemButton component={Link} to="/view-hygiene-discipline">
-            <ListItemText primary="Vệ sinh - Kỷ luật" />
-          </ListItemButton>
-          <ListItemButton component={Link} to="/view-final-competition-result">
-            <ListItemText primary="Điểm thi đua" />
-          </ListItemButton>
-        </List>
-      </Drawer>
-
-      <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8 }}>
-        <Outlet />
+    <>
+      <MainHeader />
+      <Toolbar />
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Outlet /> {/* Nơi hiển thị các trang con */}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
