627e71ffe437edac91e6a3cf243a2ed7900dc09a
Thai Huy <raziel.doan@gmail.com>
2025-09-14 22:24:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Toolbar, Box } from '@mui/material';
 import { AuthProvider } from './contexts/AuthContext';
 
+import MainHeader from './components/MainHeader';
+import Sidebar from './components/Sidebar';
 import ProtectedRoute from './components/ProtectedRoute';
 
-// Layouts
-import Layout from './layouts/Layout';
-import AdminLayout from './layouts/AdminLayout';
-import ViolationLayout from './layouts/ViolationLayout';
-import EmulationLayout from './layouts/EmulationLayout';
-
-// Pages
 import DashboardPage from './pages/DashboardPage';
+
 import LoginPage from './pages/LoginPage';
 import UnauthorizedPage from './pages/violation/UnauthorizedPage';
 import RulesPage from './pages/RulesPage';
-import ViewViolationListPage from './pages/ViewViolationListPage';
-import ViewHygieneDiscipline from './pages/ViewHygieneDiscipline';
-import ViewFinalCompetitionResult from './pages/ViewFinalCompetitionResult';
-
+import ViewAllStudentPage from './pages/ViewViolationListPage';
+import ViewHygieneDiscipline from './pages/ViewHygieneDiscipline'
+import ViewFinalCompetitionResult from './pages/ViewFinalCompetitionResult'
 // Violation
+import ViolationLayout from './layouts/ViolationLayout';
 import RecordViolationPage from './pages/violation/RecordViolationPage';
 import ViolationDetailPage from './pages/violation/ViolationDetailPage';
 import UnhandledViolationsPage from './pages/violation/UnhandledViolationsPage';
 import AllStudentPage from './pages/violation/AllViolationStudentPage';
 
 // Admin
+import AdminLayout from './layouts/AdminLayout';
 import AdminUsersPage from './pages/admin/AdminUsersPage';
 import AdminSettingPage from './pages/admin/AdminSettingPage';
 import AdminAddClassPage from './pages/admin/AdminAddClassPage';
@@ -33,22 +30,23 @@ import AcademicWeeksPage from './pages/admin/AdminWeeksSettingsPage';
 import AdminRulesPage from './pages/admin/AdminRulesPage';
 
 // Emulation
+import EmulationLayout from './layouts/EmulationLayout';
 import EnterExamScoresPage from './pages/emulation/ClassAcademicScoresPage';
 import EnterViolationScoresPage from './pages/emulation/ClassViolationScoresPage';
 import EnterClassHygieneScorePage from './pages/emulation/ClassHygieneScorePage';
 import EnterClassAttendanceSummaryPage from './pages/emulation/ClassAttendanceSummaryPage';
 import EnterClassLineUpSummaryPage from './pages/emulation/ClassLineUpSummaryPage';
 import WeeklyScoresPage from './pages/emulation/WeeklyScoresPage';
-
 function AppContent() {
   return (
     <Router>
       <CssBaseline />
+
       <Routes>
-        {/* Login route (không dùng layout) */}
+        {/* Login route (full page, no header/sidebar) */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* Admin routes */}
+        {/* Admin routes (AdminLayout renders header/sidebar internally) */}
         <Route
           path="/admin/*"
           element={
@@ -64,7 +62,7 @@ function AppContent() {
           <Route path="rules" element={<AdminRulesPage />} />
         </Route>
 
-        {/* Emulation routes */}
+        {/* Emulation routes (with global header/sidebar) */}
         <Route
           path="/emulation/*"
           element={
@@ -80,37 +78,44 @@ function AppContent() {
           <Route path="class-lineup-summaries" element={<EnterClassLineUpSummaryPage />} />
           <Route path="weekly-scores" element={<WeeklyScoresPage />} />
         </Route>
-
-        {/* Violation routes */}
+                    {/* Violation routes */}
+                    <Route
+                      path="/violation/*"
+                      element={
+                        <ProtectedRoute allowedRoles={['admin', 'student']}>
+                          <ViolationLayout />
+                        </ProtectedRoute>
+                      }
+                    >
+                      <Route path="" element={<RecordViolationPage />} />
+                      <Route path="violations/:name" element={<ViolationDetailPage />} />
+                      <Route path="unhandled" element={<UnhandledViolationsPage />} />
+                      <Route path="all-violations" element={<AllStudentPage />} />
+                      
+                    </Route>
+        {/* General app routes (with global header/sidebar) */}
         <Route
-          path="/violation/*"
+          path="*"
           element={
-            <ProtectedRoute allowedRoles={['admin', 'student']}>
-              <ViolationLayout />
-            </ProtectedRoute>
+            <Layout />
+              <MainHeader />
+              <Toolbar />
+              <Box sx={{ display: 'flex' }}>
+                <Sidebar />
+                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                  <Routes>
+                    <Route path="/" element={<DashboardPage />} />
+                    <Route path="/unauthorized" element={<UnauthorizedPage />} />
+                    <Route path="/rules" element={<RulesPage />} />
+                    <Route path="/view-all-violations" element={<ViewAllStudentPage />} />
+                    <Route path="/view-hygiene-discipline" element={<ViewHygieneDiscipline />} />
+                    <Route path="/view-final-competition-result" element={<ViewFinalCompetitionResult />} />
+                  </Routes>
+                </Box>
+              </Box>
+            </>
           }
-        >
-          <Route path="" element={<RecordViolationPage />} />
-          <Route path="violations/:name" element={<ViolationDetailPage />} />
-          <Route path="unhandled" element={<UnhandledViolationsPage />} />
-          <Route path="all-violations" element={<AllStudentPage />} />
-        </Route>
-
-        {/* General app routes (dùng Layout chung) */}
-     <Route
-  path="*"
-  element={
-      <>    
-          }
-        >
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/unauthorized" element={<UnauthorizedPage />} />
-          <Route path="/rules" element={<RulesPage />} />
-          <Route path="/view-all-violations" element={<ViewViolationListPage />} />
-          <Route path="/view-hygiene-discipline" element={<ViewHygieneDiscipline />} />
-          <Route path="/view-final-competition-result" element={<ViewFinalCompetitionResult />} />
-        </Route>
-
+        />
       </Routes>
     </Router>
   );
