96e0fa52e4031cdedc17d6f40edf81abb2320eea
Thai Huy <raziel.doan@gmail.com>
2026-08-18 20:48:49 +0700

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+
 console.log("API URL:", import.meta.env.VITE_BACKEND_URL);
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // Sử dụng biến môi trường backend
-  // withCredentials: true, // nếu backend yêu cầu cookie hoặc token
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
