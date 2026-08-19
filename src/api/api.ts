370037a9f20e7442ef6c6045aea73931d2383822
Thai Huy <raziel.doan@gmail.com>
2026-08-19 16:05:07 +0700

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,22 +1,33 @@
 import axios from "axios";
 
-console.log("API URL:", import.meta.env.VITE_BACKEND_URL);
+console.log(
+  "API URL:",
+  import.meta.env.VITE_BACKEND_URL
+);
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
+// ======================================================
+// TỰ ĐỘNG GẮN JWT TOKEN
+// ======================================================
+
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token =
+      localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization =
+        `Bearer ${token}`;
     }
 
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
